71ce3d3909557feceedbabff1a08ec711a8cf58c
Alexandre Halas <alexandrehalas@gmail.com>
2020-08-09 19:17:33 -0300

    Included openfeign
     - Included openfeign and create a proxy to comunicate with my another
     service.

--- src/main/java/com/springboot/cloud/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/springboot/cloud/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,9 @@ import com.springboot.cloud.currencyconversionservice.bean.CurrencyConversionBea
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+	
 	@GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
@@ -28,6 +32,16 @@ public class CurrencyConversionController {
 				uriVariables);
 
 		CurrencyConversionBean response = responseEntity.getBody();
+
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
+	
+	@GetMapping("currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		CurrencyConversionBean response = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
 
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
